058907293067009a5226ffa1cc6bd2813f7dae44
Unchana19 <annchanaoat@gmail.com>
2024-05-11 01:02:15 +0700

    change index

--- dev-blogs/pages/index.tsx
+++ dev-blogs/pages/index.tsx
@@ -1,12 +1,31 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { InferGetStaticPropsType, NextPage } from "next";
+import BlogCard from "../components/BlogCard";
+import { readPostsInfo } from "../lib/helper";
+import { PostApiResponse } from "../utils/types";
 
-const inter = Inter({ subsets: ["latin"] });
+export const getStaticProps = async () => {
+  const postInfo: PostApiResponse = readPostsInfo();
 
-export default function Home() {
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
-    <main className={`text-3xl font-bold underline ${inter.className}`}>
-      <h1>Hello World</h1>
-    </main>
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+      {posts.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
+      ))}
+    </div>
   );
-}
+};
+
+export default Blogs;
