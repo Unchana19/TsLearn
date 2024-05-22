02b47c2a235bdaa9c88452372254611227715ef1
Unchana19 <annchanaoat@gmail.com>
2024-05-23 02:39:31 +0700

    .

--- blogs-app/pages/_app.tsx
+++ blogs-app/pages/_app.tsx
@@ -2,10 +2,17 @@ import "@/styles/globals.css";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 interface Props {
   session: Session | null;
 }
+
+nProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeComplete", () => nProgress.done());
+Router.events.on("routeChangeError", () => nProgress.done());
 
 export default function App({ Component, pageProps }: AppProps<Props>) {
   return (
